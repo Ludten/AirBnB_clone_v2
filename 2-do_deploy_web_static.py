d3d34b2d4906fade48ac16a1e27dbbb561b724e7
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-30 00:41:51 +0100

    hbnb
    proper conditioning

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,18 +19,21 @@ def do_deploy(archive_path):
 
     if os.path.exists(archive_path) is False:
         return False
-    com_file = archive_path[9:]
-    file = archive_path[9:-4]
+    com_file = archive_path.split("/")[-1]
+    file = com_file.split(".")[0]
     if put(archive_path, '/tmp/').failed:
         return False
     if run('mkdir -p /data/web_static/releases/{}/'.format(file)).failed:
         return False
     if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            com_file, file)):
+            com_file, file)).failed:
         return False
-    if run('rm -rf /tmp/{}'.format(com_file)):
+    if run('rm -rf /tmp/{}'.format(com_file)).failed:
         return False
-    if run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.
-           format(file)):
+    if run('rm -rf /data/web_static/current').failed:
+        return False
+
+    if run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
+           format(file)).failed:
         return False
     return True
