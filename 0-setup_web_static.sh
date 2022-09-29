f16505bf37e6b8134b625952d9ed59b74d8f5b0d
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-30 00:18:57 +0100

    hbnb
    trying bugs

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 # Script for setting up web server
 apt-get -y update
-if ! dpkg -l | grep -q nginx
-then
-	apt-get -y install nginx
-fi
+apt-get -y install nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | tee /data/web_static/releases/test/index.html > /dev/null
 link="/data/web_static/current"
 ln -sf /data/web_static/releases/test/ "$link"
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+chown -hR ubuntu /data/
+chgrp -hR ubuntu /data/
 sblock="server_name\ _;\\n\\n\\tlocation\ \/hbnb_static\/\ {\\n\\t\\talias\ \/data\/web_static\/current\/;\\n\\t}\\n"
 sudo sed -i "s/server_name _;/$sblock/" /etc/nginx/sites-available/default
 service nginx restart
-exit 0
