943727389aa4c7d9bfbb8c39d053685344a20088
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-29 15:10:46 +0100

    hbnb
    script for setting up web server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Script for setting up web server
+if ! dpkg -l | grep -q nginx
+then
+	apt-get -y update
+	apt-get -y install nginx
+fi
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Holberton School" | tee /data/web_static/releases/test/index.html > /dev/null
+link="/data/web_static/current"
+if [ ! -L "$link" ]
+then
+	ln -s /data/web_static/releases/test/ "$link"
+else
+	rm "$link"
+	ln -s /data/web_static/releases/test/ "$link"
+fi
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+sblock="server_name\ _;\\n\\n\\tlocation\ \/hbnb_static\/\ {\\n\\t\\talias\ \/data\/web_static\/current\/;\\n\\t}\\n"
+sudo sed -i "s/server_name _;/$sblock/" /etc/nginx/sites-available/default
+service nginx restart
+exit 0
