6bbddc54bd12e3a5ddb9aa7c5fd3552402e2e024
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-09-30 01:42:06 +0100

    hbnb
    released first

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,11 @@
 # Script for setting up web server
 apt-get -y update
 apt-get -y install nginx
-mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | tee /data/web_static/releases/test/index.html > /dev/null
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 sblock="server_name\ _;\\n\\n\\tlocation\ \/hbnb_static\/\ {\\n\\t\\talias\ \/data\/web_static\/current\/;\\n\\t}\\n"
 sudo sed -i "s/server_name _;/$sblock/" /etc/nginx/sites-available/default
 service nginx restart
