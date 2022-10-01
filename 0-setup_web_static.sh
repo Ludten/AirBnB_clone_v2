1f64e2dd6c5a41cae10096a7cf940d4271a70208
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-10-01 17:07:28 +0100

    hbnb
    append after

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ mkdir -p /data/web_static/shared/
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | tee /data/web_static/releases/test/index.html > /dev/null
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sblock="server_name\ _;\\n\\n\\tlocation\ \/hbnb_static\/\ {\\n\\t\\talias\ \/data\/web_static\/current\/;\\n\\t}\\n"
-sudo sed -i "s/server_name _;/$sblock/" /etc/nginx/sites-available/default
+sblock="\\n\\n\\tlocation\ \/hbnb_static\/\ {\\n\\t\\talias\ \/data\/web_static\/current\/;\\n\\t}\\n"
+sudo sed "s/server_name _;/a$sblock/" /etc/nginx/sites-available/default
 service nginx restart
