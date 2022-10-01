c995724661dbae3e65466f43a243a9605cfd41f7
Oluwateniola Adegbulugbe <tenitemi@yahoo.co.uk>
2022-10-01 17:58:32 +0100

    hbnb
    fixed errors involved with server_name

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ mkdir -p /data/web_static/shared/
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | tee /data/web_static/releases/test/index.html > /dev/null
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sblock="\\n\\n\\tlocation\ \/hbnb_static\/\ {\\n\\t\\talias\ \/data\/web_static\/current\/;\\n\\t}\\n"
-sudo sed -i "/server_name _;/a $sblock" /etc/nginx/sites-available/default
+sblock="\ \\n\\n\\tlocation\ \/hbnb_static\/\ {\\n\\t\\talias\ \/data\/web_static\/current\/;\\n\\t}\\n"
+sudo sed -i "/listen 80 default_server/a $sblock" /etc/nginx/sites-available/default
 service nginx restart
